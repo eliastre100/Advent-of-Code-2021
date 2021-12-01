d614fa133b3b3de3201ee0e36286117546b74271
Anaïs FORET <anais@foret.dev>
2021-12-01 06:39:13 +0100

    Add Day 01 Part 2

--- day01/day01.go
+++ day01/day01.go
@@ -8,6 +8,14 @@ import (
 	"strconv"
 )
 
+func getDepthWindow(firstDepth int, secondDepth int, scanner *bufio.Scanner) (bool, int, int, int, int) {
+	if scanner.Scan() == false {
+		return false, firstDepth, secondDepth, 0, firstDepth + secondDepth
+	}
+	thirdDepth, _ := strconv.Atoi(scanner.Text())
+	return true, firstDepth, secondDepth, thirdDepth, firstDepth + secondDepth + thirdDepth
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -17,21 +25,32 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
-	depth, _ := strconv.Atoi(scanner.Text())
-	fmt.Printf("Start at %d\n", depth)
+	first, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	second, _ := strconv.Atoi(scanner.Text())
+
+	_, _, first, second, total := getDepthWindow(first, second, scanner)
+	third := 0
+	windowTotal, scanned := 0, true
 
 	increases := 0
-	for scanner.Scan() {
-		currentDepth, _ := strconv.Atoi(scanner.Text())
+	for {
+		scanned, first, second, third, windowTotal = getDepthWindow(first, second, scanner)
 
-		if depth < currentDepth {
-			fmt.Printf("Increased from %d to %d\n", depth, currentDepth)
+		if !scanned {
+			break
+		}
+
+		if total < windowTotal {
+			fmt.Printf("Increased from %d to %d\n", total, windowTotal)
 			increases += 1
 		}
-		depth = currentDepth
+		total = windowTotal
+		first = second
+		second = third
 	}
 
-	fmt.Printf("Increased %d times and eneded up at %d\n", increases, depth)
+	fmt.Printf(" Increased %d times and eneded up at %d\n", increases, total)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
