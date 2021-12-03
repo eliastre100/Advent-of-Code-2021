7fd1ad89da75f718af00b19340be7fd5fa7a967e
Anaïs FORET <anais@foret.dev>
2021-12-03 06:48:22 +0100

    Add day 03 part 2

--- day03/day03.rb
+++ day03/day03.rb
@@ -11,16 +11,35 @@ end
 input = InputReader.read_string(ARGV[0]).map(&:chop).map { |input| input.split("") }
 
 gamma = (0..(input[0].size - 1)).map do |idx|
-  ap "checking #{idx}"
   one = input.select { |mesurement| mesurement[idx].eql?("1") }.size
   zero = input.size - one
-  ap "have #{one} ones and #{zero} zeros"
 
   one > zero ? "1" : "0"
 end
 
-ap gamma
 epsilon = gamma.map { |gamma_bit| gamma_bit.eql?("0") ? "1" : "0" }.join
 gamma = gamma.join
 
 puts "Gama = #{gamma.to_i(2)}; Epsilon = #{epsilon.to_i(2)}; Consumption = #{gamma.to_i(2) * epsilon.to_i(2)}"
+
+def search_presence(input, position, default: "1")
+  one = input.map { |value| value[position] }.count("1")
+  return default if one == input.size.to_f / 2
+
+  one > (input.size.to_f / 2) ? "1" : "0"
+end
+
+def extract_from_value(input, position, inverse: false)
+  search = search_presence(input, position)
+  search = search.eql?("1") ? "0" : "1" if inverse
+
+  elected = input.select { |value| value[position].eql?(search) }
+  return elected.first if elected.size == 1
+
+  extract_from_value(elected, position + 1, inverse: inverse)
+end
+
+oxygen = extract_from_value(input, 0).join
+co2 = extract_from_value(input, 0, inverse: true).join
+
+puts "Oxygen = #{oxygen.to_i(2)}; CO2 = #{co2.to_i(2)}; Answer = #{oxygen.to_i(2) * co2.to_i(2)}"
