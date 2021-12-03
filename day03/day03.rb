9d8b8360f034586edc601782120b0b23095f78bc
Anaïs FORET <anais@foret.dev>
2021-12-03 08:19:20 +0100

    Clean day 03

--- day03/day03.rb
+++ day03/day03.rb
@@ -1,40 +1,44 @@
 #!/usr/bin/env ruby
 
 require_relative '../utils/input_reader'
-require "awesome_print"
 
 if ARGV.empty?
   warn 'Usage: ./day03.rb [input file]'
   exit 1
 end
 
-input = InputReader.read_string(ARGV[0]).map(&:chop).map { |input| input.split("") }
+input = InputReader.read_string(ARGV[0]).map(&:chop).map { |input| input.split('') }
 
+# Part 1
 gamma = (0..(input[0].size - 1)).map do |idx|
-  one = input.select { |mesurement| mesurement[idx].eql?("1") }.size
-  zero = input.size - one
+  input.map { |value| value[idx] }
+       .group_by { |value| value }
+       .transform_values(&:size)
+       .max_by { |value| value[1] }
+       .first
+end.join
 
-  one > zero ? "1" : "0"
-end
-
-epsilon = gamma.map { |gamma_bit| gamma_bit.eql?("0") ? "1" : "0" }.join
-gamma = gamma.join
+epsilon = gamma.gsub(/./, '0' => '1', '1' => '0')
 
 puts "Gama = #{gamma.to_i(2)}; Epsilon = #{epsilon.to_i(2)}; Consumption = #{gamma.to_i(2) * epsilon.to_i(2)}"
 
-def search_presence(input, position, default: "1")
-  one = input.map { |value| value[position] }.count("1")
-  return default if one == input.size.to_f / 2
+# Part 2
+def get_most_present(input, position, default: '1')
+  distribution = input.map { |value| value[position] }
+                      .group_by { |value| value }
+                      .transform_values(&:size)
 
-  one > (input.size.to_f / 2) ? "1" : "0"
+  return default if distribution['1'] == distribution['0']
+
+  distribution.max_by { |value| value[1] }.first
 end
 
 def extract_from_value(input, position, inverse: false)
-  search = search_presence(input, position)
-  search = search.eql?("1") ? "0" : "1" if inverse
+  elected_token = get_most_present(input, position)
+  elected_token = elected_token.eql?('1') ? '0' : '1' if inverse
 
-  elected = input.select { |value| value[position].eql?(search) }
-  return elected.first if elected.size == 1
+  elected = input.select { |value| value[position].eql?(elected_token) }
+  return elected.first if elected.one?
 
   extract_from_value(elected, position + 1, inverse: inverse)
 end
