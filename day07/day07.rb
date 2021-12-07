b202c77f697c3f42a80bb845522d7d822709f1f4
Anaïs FORET <anais@foret.dev>
2021-12-07 06:37:11 +0100

    Add day 07 part 2 (dirty)

--- day07/day07.rb
+++ day07/day07.rb
@@ -10,8 +10,6 @@ end
 
 input = InputReader.read_string(ARGV[0]).first.split(',').map(&:to_i)
 
-ap input
-
 def median(array)
   sorted = array.sort
   len = sorted.length
@@ -21,4 +19,14 @@ end
 best_pos = median(input)
 distance = input.map { |current_pos| (current_pos - best_pos).abs }.inject(&:+).to_i
 
-puts distance
+puts "Step 1: #{best_pos} best pos cost #{distance}"
+
+best_pos = (input.inject{ |sum, el| sum + el }.to_f / input.size)
+distance = input.map { |current_pos| (current_pos - best_pos).round.abs.to_i.downto(0).inject(:+) }.compact.inject(&:+)
+
+shortest = (input.min..input.max).map do |try_position|
+  [try_position, input.map { |current_pos| (current_pos - try_position).round.abs.to_i.downto(0).inject(:+) }.compact.inject(&:+)]
+end.min_by { |try| try[1] }
+
+ap shortest
+puts "Step 2: #{best_pos} best pos cost #{distance}"
