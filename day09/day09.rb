8d10ba1d69ec433eb75966c0d94f6ce411ef3ca9
Anaïs FORET <anais@foret.dev>
2021-12-09 06:28:46 +0100

    Add day 09 part 2

--- day09/day09.rb
+++ day09/day09.rb
@@ -9,6 +9,35 @@ if ARGV.empty?
 end
 
 class Cave
+  class Bassin
+    def initialize(lowest_point, cave)
+      @points = {}
+      @cave = cave
+      increase_size_from(lowest_point)
+    end
+
+    def size
+      @points.size
+    end
+
+    private
+
+    def increase_size_from(position)
+      position_value = @cave.value_at(position[0], position[1])
+      return if position_value >= 9
+
+      @points[position] = true
+      [[-1, 0], [1, 0], [0, -1], [0, 1]].each do |modifier|
+        next if @points[[position[0] + modifier[0], position[1] + modifier[1]]]
+        neighbour_value = @cave.value_at(position[0] + modifier[0], position[1] + modifier[1])
+
+        next if neighbour_value.nil? || position_value > neighbour_value
+
+        increase_size_from([position[0] + modifier[0], position[1] + modifier[1]])
+      end
+    end
+  end
+
   def initialize
     @width = 0
     @height = 0
@@ -30,10 +59,14 @@ class Cave
       end
 
       neigbouhrs.min > value
-    end.values
+    end
   end
 
-  private
+  def bassins
+    low_points.map do |starting_point, _|
+      Bassin.new(starting_point, self)
+    end
+  end
 
   def value_at(y, x)
     return 100 if x < 0 || x >= @width || y < 0 || y >= @height
@@ -45,6 +78,11 @@ end
 cave = Cave.new
 InputReader.read_string(ARGV[0]).each { |row| cave.feed(row) }
 
-low_points = cave.low_points
+low_points = cave.low_points.values
 
 ap low_points.sum +  low_points.size
+
+bassins = cave.bassins
+
+bassins_sizes = bassins.map { |bassin| bassin.size }
+ap bassins_sizes.max(3).inject(&:*)
